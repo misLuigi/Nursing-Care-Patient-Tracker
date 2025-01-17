3e1f6d5433a964e9a43ab3bf87a0811525ea7d5b
misLuigi <162457852+misLuigi@users.noreply.github.com>
2025-01-17 17:30:28 +0900

    Resumed making the Patient Meal Tracker. Added inputs, radios, etc, needed for collecting info on patient details to be stored on onject.

--- index.js
+++ index.js
@@ -4,13 +4,5 @@ const workFlowArea = document.getElementById("work-flow-console");
 console.log(workFlowArea)
 const saveInputBtn = document.getElementById("save-btn");
 console.log(saveInputBtn)
-let patientName = []
-function newBtn() {
-  workFlowArea.textContent = "What is the patient name?"
-}
-saveInputBtn.addEventListener ("click", function() {
-  patientName.push(input.value)
-  console.log(patientName)
-})
 
 
