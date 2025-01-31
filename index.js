8af1bd8705f0afdfba9b93c6da53690055ba7785
misLuigi <162457852+misLuigi@users.noreply.github.com>
2025-02-01 01:11:00 +0900

    Added the stop-meal button or the remove function on the codebase. Database will now clear out if 'Stop Meal Button' is to be click

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 // Description: This file is used to handle the logic for the patient form.
 
-// Firebase code starts here.
+// Firebase code imports starts here.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getDatabase,
           ref,
           push,
-          onValue
+          onValue,
+          remove
         } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 
 const firebaseConfig = {
     databaseURL: "https://patient-meal-tracker-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
+// Firebase variables here.
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
 const referenceInDB = ref(database, "patientData")
 
-onValue(referenceInDB, function(snapshot) {
-    console.log(snapshot.val())
-})
-
-// necessary variables and elements.
+// Necessary HTML variables and elements.
 const patientName = document.getElementById("patient-name");
 console.log(patientName);
 const patientRoomNumber = document.getElementById("patient-room-number");
@@ -41,11 +39,28 @@ const addBtn = document.getElementById("add-btn");
 console.log(addBtn);
 const msgPanel = document.getElementById("msg");
 console.log(msgPanel);
+const stopMealBtn = document.getElementById("stop-meal-btn");
+console.log(stopMealBtn);
 
-// Array to store the patient data
+// onValue section. 
+onValue(referenceInDB, function(snapshot) {
+  const snapshotDoesExist = snapshot.exists()
+  if (snapshotDoesExist) {
+    const snapshotValue = snapshot.val()
+    const data = Object.values(snapshotValue)
+    msgPanel.innerHTML = snapshotValue;
+    console.log(snapshotValue)
+  } else {
+    console.log("No, content!")
+  }
+  
+})
 
 
-// Add patient event
+// Render data into the html (data cells) to visualize.
+
+
+// Add-patient event.
 const addPatient = (e) => {
   e.preventDefault();
   let patient = {
@@ -65,9 +80,14 @@ const addPatient = (e) => {
 
   // Display the message
   //console.warn('added', {patient});
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   addBtn.addEventListener('click', addPatient);
 });
 
+// Stop-meal event.
+stopMealBtn.addEventListener("click", function() {
+  remove(referenceInDB)
+})
