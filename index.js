1170b12fb4f7aec226f05d49cbcbd0c97b714ad2
misLuigi <162457852+misLuigi@users.noreply.github.com>
2025-02-02 12:35:18 +0900

    Abandoned the 'Patient Meal Tracker' and swap it with 'Spending Tracker' instead as it is near to my needs and solves my problem than the latter. Also changed all the firebase links and database, renamed the README file, and overhauled everything. Added the structure of the website app and added a modal and functionality in the app. Not done yet

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-// Description: This file is used to handle the logic for the patient form.
-
 // Firebase code imports starts here.
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js"
 import { getDatabase,
           ref,
           push,
           onValue,
           remove
-        } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+        } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: "https://patient-meal-tracker-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://spending-tracker-da10e-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 // Firebase variables here.
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-const referenceInDB = ref(database, "patientData")
+const referenceInDB = ref(database, "inputData")
+*/
+// JavaScript logic starts here.
+// Necessary variables and elements:
 
-// Necessary HTML variables and elements.
-const patientName = document.getElementById("patient-name");
-console.log(patientName);
-const patientRoomNumber = document.getElementById("patient-room-number");
-console.log(patientRoomNumber);
-const secondThirdFloor = document.getElementById("second-or-third-floor");
-console.log(secondThirdFloor);
-const ricePreference = document.getElementById("rice-preference");
-console.log(ricePreference);
-const typeOfFood = document.getElementById("type-of-food");
-console.log(typeOfFood);
-const prohibition = document.getElementById("prohibition");
-console.log(prohibition);
-const allergies = document.getElementById("allergies");
-console.log(allergies);
-const options = document.getElementById("options");
-console.log(options);
-const addBtn = document.getElementById("add-btn");
-console.log(addBtn);
-const msgPanel = document.getElementById("msg");
-console.log(msgPanel);
-const stopMealBtn = document.getElementById("stop-meal-btn");
-console.log(stopMealBtn);
+const addSpendingBtn = document.getElementById("add-spending-el")
+console.log(addSpendingBtn)
+const modalOverlay = document.querySelector(".modal-overlay")
+console.log(modalOverlay)
+const closeOverlayBtn = document.querySelector(".close-overlay-btn")
+console.log(closeOverlayBtn)
+
+addSpendingBtn.addEventListener("click", function() {
+  modalOverlay.showModal();
+})
+
+closeOverlayBtn.addEventListener("click", function() {
+  modalOverlay.close();
+})
 
 // onValue section. 
-onValue(referenceInDB, function(snapshot) {
+/*onValue(referenceInDB, function(snapshot) {
   const snapshotDoesExist = snapshot.exists()
   if (snapshotDoesExist) {
     const snapshotValue = snapshot.val()
-    const data = Object.values(snapshotValue)
-    msgPanel.innerHTML = snapshotValue;
-    console.log(snapshotValue)
+    console.log(snapshotValue);
+    const personsData = Object.values(snapshotValue)
+    console.log(personsData)
+    let result = personsData.filter(function() {
+      typeOfFood === "Joushouku"
+    })
+    console.log(result);
+    console.log(result.length)
   } else {
     console.log("No, content!")
   }
@@ -90,4 +87,4 @@ document.addEventListener('DOMContentLoaded', () => {
 // Stop-meal event.
 stopMealBtn.addEventListener("click", function() {
   remove(referenceInDB)
-})
+})*/
