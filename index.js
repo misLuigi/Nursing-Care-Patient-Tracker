1246e76b3a08ee894e05733a0183f5caeb5d480a
misLuigi <162457852+misLuigi@users.noreply.github.com>
2025-02-07 14:55:36 +0900

    Set up the firebase database, rendered the added spending, converted the object through loop.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Firebase code imports starts here.
-/*import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js"
 import { getDatabase,
           ref,
           push,
@@ -15,16 +15,19 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const referenceInDB = ref(database, "inputData")
-*/
-// JavaScript logic starts here.
-// Necessary variables and elements:
 
+console.log(database)
+
+// JavaScript logic starts here.
+// Necessary variables and elements.
 const addSpendingBtn = document.getElementById("add-spending-el")
 console.log(addSpendingBtn)
 const modalOverlay = document.querySelector(".modal-overlay")
 console.log(modalOverlay)
 const closeOverlayBtn = document.querySelector(".close-overlay-btn")
 console.log(closeOverlayBtn)
+const deleteDataBtn = document.getElementById("delete-btn-el")
+console.log(deleteDataBtn)
 
 // variables and elements of form and its values.
 const nameOfItem = document.getElementById("name-of-item")
@@ -33,22 +36,20 @@ const placeToBuy = document.getElementById("where")
 console.log(placeToBuy)
 const cost = document.getElementById("cost-of-item")
 console.log(cost)
-
-
 const submitBtn = document.querySelector(".submit-btn")
 console.log(submitBtn)
+const panel = document.getElementById("panel-el")
 
 // opens the modal for adding new item.
 addSpendingBtn.addEventListener("click", function() {
   modalOverlay.showModal();
 })
-// closes the modal. but, clicking the 'submit' button will also do the same thing.
+// clicking the 'X' button closes the modal. But, clicking the 'submit' button will also do the same thing.
 closeOverlayBtn.addEventListener("click", function() {
   modalOverlay.close();
 })
 
 //functions:
-let newData = [] // data will be store in this array when addItem event is successful.
 const addItem = (event) => {
   event.preventDefault();
   const paymentMethod = document.querySelector('input[name = "payment-method"]:checked') // this get the selected value of the radio button. and it should be inside the scope of this event.
@@ -60,14 +61,14 @@ const addItem = (event) => {
     methodOfPayment: paymentMethod ? paymentMethod.value : null // check if one of the radio button is selected.
   }
 
-  newData.push(newItems)
-  console.log(newData)
+  push(referenceInDB, newItems)
   document.forms[0].reset();
 
   modalOverlay.close();
   
-  const panel = document.getElementById("panel-el")
-console.log(panel.textContent = JSON.stringify(newData))
+  
+  
+
 }
 // this listen for the event that when submit button is clicked, then the addItem event will be push to newData array.
 document.addEventListener('DOMContentLoaded', () => {
@@ -77,56 +78,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // onValue section. 
-/*onValue(referenceInDB, function(snapshot) {
+onValue(referenceInDB, function(snapshot) {
   const snapshotDoesExist = snapshot.exists()
   if (snapshotDoesExist) {
     const snapshotValue = snapshot.val()
     console.log(snapshotValue);
-    const personsData = Object.values(snapshotValue)
-    console.log(personsData)
-    let result = personsData.filter(function() {
-      typeOfFood === "Joushouku"
-    })
-    console.log(result);
-    console.log(result.length)
+    const itemsData = Object.values(snapshotValue)
+    console.log(itemsData)
+    render(itemsData)
   } else {
     console.log("No, content!")
   }
-  
 })
 
-
 // Render data into the html (data cells) to visualize.
-
-
-// Add-patient event.
-const addPatient = (e) => {
-  e.preventDefault();
-  let patient = {
-    name: patientName.value,
-    roomNumber: patientRoomNumber.value,
-    floor: secondThirdFloor.value,
-    rice: ricePreference.value,
-    typeOfFood: typeOfFood.value,
-    prohibition: prohibition.value,
-    allergies: allergies.value,
-    options: options.value
+function render(itemsData) {
+  let listItems = ""
+  for (let i=0; i < itemsData.length; i++) {
+    listItems += `
+    <li>
+    Name: ${itemsData[i].name}, 
+    Store: ${itemsData[i].store}, 
+    Payment Method: ${itemsData[i].paymentMethod}, 
+    Price: ${itemsData[i].price}</li>
+    `
   }
-  
-  push(referenceInDB, patient)
-  document.forms[0].reset();
-
-
-  // Display the message
-  //console.warn('added', {patient});
-
+  panel.innerHTML = listItems;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  addBtn.addEventListener('click', addPatient);
-});
 
 // Stop-meal event.
-stopMealBtn.addEventListener("click", function() {
+deleteDataBtn.addEventListener("click", function() {
   remove(referenceInDB)
-})*/
+  panel.textContent = "Data Successfuly Deleted!"
+})
